e6185cd8362c705c2b9e67dd7ab8090bb65f4f84
aylin828 <74410042+aylin828@users.noreply.github.com>
2020-12-11 11:28:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Daily ueberSchrift = "Daily Scrum "/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Daily from './components/Daily.vue'
+
+export default {
+  name: 'App',
+  components: {
+   HelloWorld,
+    Daily
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
